b0ef8260d986beca547b38c43a06155849e042bc
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-12-07 19:54:26 -0500

    ws

--- client/src/stores.js
+++ client/src/stores.js
@@ -1,7 +1,7 @@
 import { writable, readable } from 'svelte/store';
 import { io } from "socket.io-client";
 
-export const Socket = readable(io());
+export const Socket = readable(io(`ws://${window.location.host}`));
 
 export const started = writable(false);
 export const game = writable();
